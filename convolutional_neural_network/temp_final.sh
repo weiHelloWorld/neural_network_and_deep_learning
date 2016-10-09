affbddf3a9003020b8339604d8b9bf49bf7ce3f2
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-10-09 17:40:09 -0500

    updated scripts

--- convolutional_neural_network/temp_final.sh
+++ convolutional_neural_network/temp_final.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python CIFAR10_chainer.py 30  --num_fully_conn 3 --nums_conv_sets 2,2,2 --data_augmentation 2 --optimizer Adam >> results_final.txt
+python CIFAR10_chainer.py 50  --num_fully_conn 2 --nums_conv_sets 2,2,2 --data_augmentation 2 >> results_final.txt
